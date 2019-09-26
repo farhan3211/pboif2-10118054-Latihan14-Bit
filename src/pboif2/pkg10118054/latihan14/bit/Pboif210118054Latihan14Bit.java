e11678a6c4c10c603331498a27c319ce47bb469c
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:26:37 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan14/bit/Pboif210118054Latihan14Bit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan14.bit;
+
+/**
+ *
+ * @author ASUS
+ * nama   :Farhan Milardi
+ * kelas  :IF-2
+ * nim    :10118054
+ */
+public class Pboif210118054Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     * int a = 60;
+        int b = 13;
+        int c = 0;
+        c = a & b;
+        
+        System.out.println("a & b = " + c);
+        c = a | b;
+        
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        
+        System.out.println("a ^ b " + c);
+        c = ~a;
+        
+        System.out.println("~a = " + c);
+        c = a << 2;
+        
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        
+        System.out.println("a >> 2 " + c);
+
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
